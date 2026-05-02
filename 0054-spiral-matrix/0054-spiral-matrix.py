6d618927c262d4de1b4aa078ac14a754aa73f0ed
AKSHAY KUMAR A S <162968906+Akshaykumar1511@users.noreply.github.com>
2026-05-02 19:10:59 +0530

    Time: 0 ms (100%), Space: 19.3 MB (34.16%) - LeetHub

--- 0054-spiral-matrix/0054-spiral-matrix.py
+++ 0054-spiral-matrix/0054-spiral-matrix.py
@@ -1,45 +1,46 @@
 class Solution:
     def spiralOrder(self, matrix: List[List[int]]) -> List[int]:
-        n,m=len(matrix),len(matrix[0])
-        ans=[]
-        up,down,left,right=0,1,2,3
-        direction=right
+        rows,cols=len(matrix),len(matrix[0])
+        left,right,up,down=0,1,2,3
+        direction = right
         i,j=0,0
+        upwall=0
+        downwall=rows
+        leftwall=-1
+        rightwall=cols
+        res=[]
 
-        UP_WALL=0
-        DOWN_WALL=n
-        RIGHT_WALL=m
-        LEFT_WALL=-1
-        while len(ans)!=n*m:
+        while rows*cols!=len(res):
             if direction==right:
-                while j<RIGHT_WALL:
-                    ans.append(matrix[i][j])
+                while j<rightwall:
+                    res.append(matrix[i][j])
                     j+=1
-                i,j=i+1,j-1
-                RIGHT_WALL-=1
+                rightwall-=1
+                i+=1
+                j-=1
                 direction=down
-
             elif direction==down:
-                while i<DOWN_WALL:
-                    ans.append(matrix[i][j])
+                while i<downwall:
+                    res.append(matrix[i][j])
                     i+=1
-                i,j=i-1,j-1
-                DOWN_WALL-=1
+                downwall-=1
+                i-=1
+                j-=1
                 direction=left
-
             elif direction==left:
-                while j>LEFT_WALL:
-                    ans.append(matrix[i][j])
+                while j>leftwall:
+                    res.append(matrix[i][j])
                     j-=1
-                i,j=i-1,j+1
-                LEFT_WALL+=1
+                leftwall+=1
+                j+=1
+                i-=1
                 direction=up
-
             else:
-                while i>UP_WALL:
-                    ans.append(matrix[i][j])
+                while i>upwall:
+                    res.append(matrix[i][j])
                     i-=1
-                i,j=i+1,j+1
-                UP_WALL+=1
+                upwall+=1
+                i+=1
+                j+=1
                 direction=right
-        return ans
+        return res
