00076028759f7aed803ec65928ed896f65115625
AKSHAY KUMAR A S <162968906+Akshaykumar1511@users.noreply.github.com>
2026-06-02 23:10:48 +0530

    Time: 3 ms (61.95%), Space: 19.1 MB (99.58%) - LeetHub

--- 1189-maximum-number-of-balloons/1189-maximum-number-of-balloons.py
+++ 1189-maximum-number-of-balloons/1189-maximum-number-of-balloons.py
@@ -2,15 +2,14 @@ from collections import defaultdict
 
 class Solution:
     def maxNumberOfBalloons(self, text: str) -> int:
-        a=defaultdict(int)
+        a={}
         for i in text:
             if i in a:
                 a[i]=a[i]+1
             else:
                 a[i]=1
-        
-
-        if any(a)==True:
+        b={"a","b","l","o","n"}
+        if all(ch in a for ch in "ballon"):
             return min(a["b"],a["a"],a["l"]//2,a["o"]//2,a["n"])
         else:
             return 0
