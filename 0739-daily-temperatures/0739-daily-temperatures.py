fb3f5e25ce0cd4b5e6c00c41217f5aaae7f1ad65
AKSHAY KUMAR A S <162968906+Akshaykumar1511@users.noreply.github.com>
2026-08-01 11:53:41 +0530

    Time: 90 ms (70.1%), Space: 28.7 MB (46.45%) - LeetHub

--- 0739-daily-temperatures/0739-daily-temperatures.py
+++ 0739-daily-temperatures/0739-daily-temperatures.py
@@ -1,26 +1,10 @@
 class Solution:
     def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
-        # brute force n^2 not accepted
-        # l=len(temperatures)
-        # fin=[0]*l
-        # for i in range(l):
-        #     cnt=0
-        #     j=i+1
-        #     while j<l:
-        #         if temperatures[i]>=temperatures[j]:
-        #             cnt+=1
-        #             j+=1
-        #         else:
-        #             fin[i]=cnt+1
-        #             break
-        # return fin
-        temps=temperatures
-        n=len(temps)
-        ans=[0]*n
+        a=[0]*len(temperatures)
         stk=[]
-        for i,v in enumerate(temps):
-            while stk and stk[-1][1]<v:
-                stk_i,stk_v=stk.pop()
-                ans[stk_i]=i-stk_i
-            stk.append((i,v))
-        return ans
+        for i in range(len(temperatures)):
+            while stk and temperatures[i]>temperatures[stk[-1]]:
+                b=stk.pop()
+                a[b]=(i-b)
+            stk.append(i)
+        return a
