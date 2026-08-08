e09b79f5ef4dbb38cea4b5eb10e291031e334806
AKSHAY KUMAR A S <162968906+Akshaykumar1511@users.noreply.github.com>
2026-08-08 16:30:22 +0530

    Time: 0 ms (100%), Space: 22.2 MB (61.4%) - LeetHub

--- 0230-kth-smallest-element-in-a-bst/0230-kth-smallest-element-in-a-bst.py
+++ 0230-kth-smallest-element-in-a-bst/0230-kth-smallest-element-in-a-bst.py
@@ -6,42 +6,15 @@
 #         self.right = right
 class Solution:
     def kthSmallest(self, root: Optional[TreeNode], k: int) -> int:
-        # ans=[]
-        # def traverse(root):
-        #     if not root:
-        #         return
-        #     if root.val not in ans:
-        #         ans.append(root.val)
-        #     traverse(root.left)
-        #     traverse(root.right)
-        # traverse(root)
-        # ans.sort()
-        # return ans[k-1]
-        
-        #method 2
-        # count=[k]
-        # ans=[0]
-        # def rec(root):
-        #     if not root:
-        #         return
-        #     rec(root.left)
-        #     if count[0]==1:
-        #         ans[0]=root.val
-        #     count[0]-=1
-        #     if count[0]>0:
-        #         rec(root.right)
-        # rec(root)
-        # return ans[0]
-
-        count=[k]
-        def rec(root):
-            if not root:
+        ans=[None]
+        a=[k]
+        def helper(node):
+            if not node:
                 return None
-            ans=rec(root.left)
-            if ans is not None:
-                return ans
-            count[0]-=1
-            if count[0]==0:
-                return root.val
-            return rec(root.right)
-        return rec(root)
+            helper(node.left)
+            a[0]-=1
+            if a[0]==0:
+                ans[0]=node.val
+            helper(node.right)
+        helper(root)
+        return ans[0]
