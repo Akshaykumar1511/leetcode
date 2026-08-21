1fdf62b785c01371f156d6bed1683e9c726747ec
AKSHAY KUMAR A S <162968906+Akshaykumar1511@users.noreply.github.com>
2026-08-21 23:12:35 +0530

    Time: 13 ms (88.98%), Space: 20.6 MB (53.24%) - LeetHub

--- 0695-max-area-of-island/0695-max-area-of-island.py
+++ 0695-max-area-of-island/0695-max-area-of-island.py
@@ -1,20 +1,14 @@
 class Solution:
     def maxAreaOfIsland(self, grid: List[List[int]]) -> int:
-        best=0
         def back(i,j):
-            if i<0 or i>=n or j<0 or j>=m or grid[i][j]==0:
+            if i>=n or j>=m or i<0 or j<0 or grid[i][j]==0:
                 return 0
             grid[i][j]=0
-            below=back(i+1,j)
-            right=back(i,j+1)
-            up=back(i-1,j)
-            left=back(i,j-1)
-            return 1+below+right+left+up
-
+            return 1+back(i+1,j)+back(i-1,j)+back(i,j+1)+back(i,j-1)
         n,m=len(grid),len(grid[0])
+        maxx=0
         for i in range(n):
             for j in range(m):
                 if grid[i][j]==1:
-                    cnt=back(i,j)
-                    best=max(cnt,best)
-        return best
+                    maxx=max(back(i,j),maxx)
+        return maxx
