1b35fa3fe2f2ab7c643f7f3788d661f48025e7e0
AKSHAY KUMAR A S <162968906+Akshaykumar1511@users.noreply.github.com>
2026-08-25 18:44:47 +0530

    Time: 0 ms (100%), Space: 19.2 MB (79.2%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.py
+++ 0994-rotting-oranges/0994-rotting-oranges.py
@@ -1,30 +1,30 @@
 from collections import deque
 class Solution:
     def orangesRotting(self, grid: List[List[int]]) -> int:
-        EMPTY,FRESH,ROTTEN=0,1,2
-        d=deque()
-        fo=0
+        FRESH,EMPTY,ROTTEN=1,0,2
+        q=deque()
         n,m=len(grid),len(grid[0])
+        minn=-1
+        frec=0
         for i in range(n):
             for j in range(m):
                 if grid[i][j]==FRESH:
-                    fo+=1
-                elif grid[i][j]==ROTTEN:
-                    d.append((i,j))
-        if fo==0:
+                    frec+=1
+                if grid[i][j]==ROTTEN:
+                    q.append((i,j))
+        if frec==0:
             return 0
-        mo=-1
-        while d:
-            l=len(d)
-            mo+=1
-            for _ in range(l):
-                i,j=d.popleft()
-                for r,c in ((i,j+1),(i,j-1),(i-1,j),(i+1,j)):
+        while q:
+            lenq=len(q)
+            minn+=1
+            for _ in range(lenq):
+                i,j=q.popleft()
+                for r,c in ((i+1,j),(i-1,j),(i,j-1),(i,j+1)):
                     if 0<=r<n and 0<=c<m and grid[r][c]==FRESH:
-                        d.append((r,c))
+                        q.append((r,c))
+                        frec-=1
                         grid[r][c]=ROTTEN
-                        fo-=1
-        if fo==0:
-            return mo
+        if frec==0:
+            return minn
         else:
             return -1
