2bc4444e229681ccc3688e5945ce95ae0071740e
AKSHAY KUMAR A S <162968906+Akshaykumar1511@users.noreply.github.com>
2026-09-24 20:24:44 +0530

    Time: 651 ms (80.51%), Space: 0B (100%) - LeetHub

--- 1731-the-number-of-employees-which-report-to-each-employee/1731-the-number-of-employees-which-report-to-each-employee.sql
+++ 1731-the-number-of-employees-which-report-to-each-employee/1731-the-number-of-employees-which-report-to-each-employee.sql
@@ -1,21 +1,16 @@
 # Write your MySQL query statement below
-with manager_stats as (select reports_to as employee_id, name ,count(reports_to) as reports_count, round(avg(age),0) as average_age
-from employees
-where reports_to is not null
-group by reports_to)
+-- with manager_stats as (select reports_to as employee_id, name ,count(reports_to) as reports_count, round(avg(age),0) as average_age
+-- from employees
+-- where reports_to is not null
+-- group by reports_to)
 
-select e.employee_id, e.name,m.reports_count,m.average_age
-from employees e
-join manager_stats m on m.employee_id=e.employee_id
+-- select e.employee_id, e.name,m.reports_count,m.average_age
+-- from employees e
+-- join manager_stats m on m.employee_id=e.employee_id
+-- order by e.employee_id
+
+select e.employee_id, e.name, count(m.employee_id) as reports_count, round(avg(m.age),0) as average_age
+from Employees e
+join Employees m on e.employee_id=m.reports_to
+group by e.employee_id,e.name
 order by e.employee_id
-
-
--- SELECT 
---     m.employee_id, 
---     m.name, 
---     COUNT(e.employee_id) AS reports_count, 
---     ROUND(AVG(e.age), 0) AS average_age
--- FROM Employees e
--- JOIN Employees m ON e.reports_to = m.employee_id
--- GROUP BY m.employee_id, m.name
--- ORDER BY m.employee_id;
